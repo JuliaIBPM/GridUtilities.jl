db7adcc06f09f1c756971c73f79c9ee03a4e0fdb
Jeff Eldredge <jdeldre@g.ucla.edu>
2020-06-24 07:43:01 -0700

    Updated StorePlan API

--- src/storage.jl
+++ src/storage.jl
@@ -54,12 +54,14 @@ function _writelist(R::WritePlan,varlist...)
 end
 
 """
-    StorePlan(min_t,max_t,store_Δt,v[;htype=RegularHistory])
+    StorePlan(sample_time,varlist[;htype=RegularHistory,min_time=-Inf,max_time=Inf])
 
-Create a plan for storing history data. The storage of data is specified to
-start at time unit `min_t` and to proceed until (and including) `max_t`, and
-is stored every `store_Δt` time units. The list of variables to be stored is
-specified as a list of variables `v`. Tuple-type variables are unwrapped
+Create a plan for storing history data. The storage of data is specied to occur
+at a sample interval given by `sample_time`. Optionally, one can specify the
+initial time `min_time` and final time `max_time` (included) for a sampling window.
+These default to all times. The list of variables to be stored is
+specified as a list of variables `varlist`, provided as comma-separated named pairs,
+e.g. `"varname1"-> v1,"varname2"-> v2`. Tuple-type variables are unwrapped
 into separate storage. The optional argument `htype` can be used to set the
 history data to `PeriodicHistory` or `RegularHistory` (the default). In the
 case of `PeriodicHistory`, the data is assumed to repeat with a period equal
@@ -70,7 +72,7 @@ struct StorePlan{H<:HistoryType}
     max_t::Float64
     store_Δt::Float64
     vardict::Dict
-    StorePlan(min_t,max_t,store_Δt,varlist...;htype=RegularHistory) = new{htype}(min_t,max_t,store_Δt,_get_typedict(varlist))
+    StorePlan(store_Δt,varlist...;htype=RegularHistory,min_time=-Inf,max_time=Inf) = new{htype}(min_time,max_time,store_Δt,_get_typedict(varlist))
 end
 
 """
